804db128726f9645c2d780517a157f8311ddb84e
Mohshaikh23 <mohsin.shaikh324@gmail.com>
2024-06-07 05:26:16 +0530

    Session 2 completed

--- Flask for Machine Learning/Session 1 - Introduction to Flask/app2.py
+++ Flask for Machine Learning/Session 1 - Introduction to Flask/app2.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+import streamlit as st
 
 app = Flask(__name__)
 
@@ -7,13 +8,19 @@ def home():
     # return "<h2> Welcome bro </h2>"
     return render_template("./index.html")
 
-@app.route("/about", methods=["GET", "POST"])
-def about():
-    data = {'name': None}
+@app.route("/about/<name>")
+def about(name):
+    data = {'name': name}
     if request.method == "POST":
         name = request.form.get("name")
         data = {'name': name}
+    else :
+        name = "Mate"
     return render_template("about.html", data=data)
+
+@app.route("/dashboard")
+def dashoard():
+    return render_template("dashboard.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
