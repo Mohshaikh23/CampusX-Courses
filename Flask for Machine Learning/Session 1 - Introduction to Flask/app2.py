fb47ac5676d554fa432599e7e44d308e77467520
Mohshaikh23 <mohsin.shaikh324@gmail.com>
2024-06-06 21:40:00 +0530

    session1 completed

--- Flask for Machine Learning/Session 1 - Introduction to Flask/app2.py
+++ Flask for Machine Learning/Session 1 - Introduction to Flask/app2.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,13 @@ def home():
     # return "<h2> Welcome bro </h2>"
     return render_template("./index.html")
 
-@app.route("/about")
+@app.route("/about", methods=["GET", "POST"])
 def about():
-    return render_template("./about.html")
+    data = {'name': None}
+    if request.method == "POST":
+        name = request.form.get("name")
+        data = {'name': name}
+    return render_template("about.html", data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
